7234a26c3a1d5350345e5e29848494c633cba36a
Talles Corradini <tallescorradini@gmail.com>
2022-05-27 21:29:14 -0300

    feat: list posts from Prismic with pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import { GetStaticProps } from 'next';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import Link from 'next/link';
 
+import { useState } from 'react';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +28,58 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const { results, next_page } = postsPagination;
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+  const [posts, setPosts] = useState(results);
+  const [nextPage, setNextPage] = useState(next_page);
 
-//   // TODO
-// };
+  async function handleLoadMore(): Promise<void> {
+    const response = await fetch(nextPage, {
+      method: 'GET',
+    });
+    const { results: fetchedPosts, next_page: fetchedNextPage } =
+      await response.json();
+
+    setPosts(prevState => [...prevState, ...fetchedPosts]);
+    setNextPage(fetchedNextPage);
+  }
+
+  return (
+    <div>
+      <ul>
+        {posts.map(post => (
+          <li key={post.uid}>
+            <h2>
+              <Link href={`/post/${post.uid}`}>
+                <a>{post.data.title}</a>
+              </Link>
+            </h2>
+            <p>{post.data.subtitle}</p>
+            <p>{post.data.author}</p>
+            <p>
+              {format(new Date(post.first_publication_date), 'd MMM yyyy', {
+                locale: ptBR,
+              })}
+            </p>
+          </li>
+        ))}
+      </ul>
+
+      {nextPage !== null ? (
+        <button type="button" onClick={handleLoadMore}>
+          Carregar mais posts
+        </button>
+      ) : null}
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('posts', { pageSize: 1 });
+
+  return {
+    props: { postsPagination: postsResponse },
+  };
+};
