d65851bdfd2646f6e7ab45584d69a28295ba6ff4
Talles Corradini <tallescorradini@gmail.com>
2022-05-27 21:45:58 -0300

    feat(Header): add logo and link to homepage

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,20 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Header(): JSX.Element {
+  return (
+    <header>
+      <Link href="/">
+        <a>
+          <Image
+            alt="logo"
+            src="/Logo.svg"
+            layout="fixed"
+            width="239"
+            height="26"
+          />
+        </a>
+      </Link>
+    </header>
+  );
 }
