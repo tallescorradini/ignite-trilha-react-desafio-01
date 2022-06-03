02659bc35f139f060aeca2bf90241bc2de403240
Talles Corradini <tallescorradini@gmail.com>
2022-06-03 16:23:08 -0300

    fix(Header): make component overlap other elements

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,18 +5,20 @@ import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
-    <header className={`center inline-gutters-16 ${styles.header}`}>
-      <Link href="/">
-        <a>
-          <Image
-            alt="logo"
-            src="/Logo.svg"
-            layout="fixed"
-            width="239"
-            height="26"
-          />
-        </a>
-      </Link>
+    <header className={`inline-gutters-16 ${styles.header}`}>
+      <div className={`center ${styles.headerContainer}`}>
+        <Link href="/">
+          <a>
+            <Image
+              alt="logo"
+              src="/Logo.svg"
+              layout="fixed"
+              width="239"
+              height="26"
+            />
+          </a>
+        </Link>
+      </div>
     </header>
   );
 }
